9d72ec532cbb98d5fb6c58c3cd51400a3338a2e1
Katrina Velez <katrinamvelez@gmail.com>
2016-06-29 10:41:39 -0400

    trying to get messageID

--- gapi/taskhistory.js
+++ gapi/taskhistory.js
@@ -1,9 +1,6 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log('trying to read message');
     if (request.type === 'read message') {
-        console.log('taskhistory in background script');
-        console.log('trying to access gmail api: ', gapi.client.gmail);
-        sendResponse(gapi.client.gmail.users.threads.get({
+        sendResponse(gapi.client.gmail.users.messages.get({
         	'id': request.threadId,
             'userId': 'me',
             'format': 'raw'
--- js/threadview/taskhistory.js
+++ js/threadview/taskhistory.js
@@ -1,20 +1,22 @@
 InboxSDK.load('1.0', 'sdk_CapstoneIDK_aa9966850e').then(function(sdk) {
-	sdk.Conversations.registerThreadViewHandler(function(threadView){
-		var taskHistory = document.createElement('div');
-		taskHistory.style = "height: 200px; width: 200px";
-		threadView.addSidebarContentPanel({
-			el : taskHistory,
-			title: 'Task History',
-			iconUrl: 'https://cdn3.iconfinder.com/data/icons/website-panel-icons/128/test1-13-512.png'
-		})
-	})
+    sdk.Conversations.registerThreadViewHandler(function(threadView) {
+        var taskHistory = document.createElement('div');
+        taskHistory.style = "height: 200px; width: 200px";
+        threadView.addSidebarContentPanel({
+            el: taskHistory,
+            title: 'Task History',
+            iconUrl: 'https://cdn3.iconfinder.com/data/icons/website-panel-icons/128/test1-13-512.png'
+        })
+    })
 
-	sdk.Conversations.registerThreadViewHandler(function(threadView){
-		chrome.runtime.sendMessage({
-			type: 'read message',
-			threadId: threadView.getThreadID()
-		}, function(response){
-			console.log('now trying to get metadata: ', response);
-		})
-	});
+    sdk.Conversations.registerThreadViewHandler(function(threadView) {
+        chrome.runtime.sendMessage({
+            type: 'read message',
+            threadId: threadView.getThreadID()
+        }, function(response) {
+
+            console.log('now trying to get metadata: ', response.body);
+
+        })
+    });
 });
