86e0eec84c845c4b83e33f8cbc0346af7432e250
Kathy Chang <emailkathy@gmail.com>
2016-06-30 18:35:21 -0400

    still working

--- angular/app.js
+++ angular/app.js
@@ -1,25 +1,27 @@
 // var $ = require('jquery');
 var app = angular.module('thing', ['firebase', 'ui.router']);
 
-<<<<<<< HEAD
 app.controller('DashboardCtrl', function($scope) {
 	$scope.kathy = "kathy yay";
-	console.log("hello from inside app.js");
+	console.log("hello from inside app.js DashboardCtrl");
 });
 
 var assignapp = angular.module('shazzam', ['firebase']);
-console.log("hello from outside assignapp controller");
+console.log("hello from outside app.js AssignCtrl");
 
 assignapp.controller('AssignCtrl', function($scope, $firebaseArray) {
-	var ref = firebase.database().ref('/members'); 
 	console.log("hello from inside assignapp.js AssignCtrl");
+
+	var ref = firebase.database().ref('/members'); 
 	// $scope.members = ['Belinda', 'Katrina', 'Rina', 'Kathy'];
 	$scope.members = $firebaseArray(ref);
 	console.log("$scope.members: ", $scope.members);
+	console.log("$scope.members length: ", $scope.members.length);
 	$scope.assignsubmit = function() {
 		console.log("you assignsubmitted something but not really");
 	}
-=======
+});
+
 // var insert = '';
 // // var el = document.createElement("div");
 // fetch(chrome.extension.getURL('/templates/dashboard-home.html'))
@@ -100,6 +102,5 @@ app.controller('TemplatesCtrl', function($scope, $firebaseArray, $state) {
 });
 
 app.controller('SettingsCtrl', function($scope, $firebaseArray, $state) {
->>>>>>> 89fcb62adf5399f8b8c2835ab18d21bab6322f29
 	
 });
