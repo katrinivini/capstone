5f36245a1275960849385944380d58d43eb6c319
Katrina Velez <katrinamvelez@gmail.com>
2016-07-01 00:57:30 -0400

    able to update comment database correctly

--- js/threadview/comment.js
+++ js/threadview/comment.js
@@ -1,34 +1,94 @@
 var $ = require('../myapp.js').$;
 // console.log('$', $);
 var messages = require('../myapp.js').messages;
-var comments = document.createElement('div');
-var message;
-var person;
-var hash;
+
 InboxSDK.load('1.0', 'sdk_CapstoneIDK_aa9966850e').then(function(sdk) {
     sdk.Conversations.registerThreadViewHandler(function(threadView) {
-        //make the comments sidebar content panel
+        var comments = document.createElement('div');
+        var message;
+        var person;
+        var hash;
+        $(comments).load(chrome.extension.getURL('/templates/comment.html'), function(page) {
+            var submit = document.getElementById('submit');
+            console.log(document.getElementById('addComment'));
+            submit.addEventListener('click', function(event) {
+                event.preventDefault();
+                chrome.runtime.sendMessage({
+                    type: 'add comment',
+                    threadId: threadView.getThreadID()
+                }, function(response) {
+                    // var foo = {};
+                    var data;
+                    hash = response;
+                    person = sdk.User.getAccountSwitcherContactList()[0].name;
+                    Promise.resolve(messages.once('value', function(snapshot) {
+                            data = snapshot.val();
+                            // foo[hash] = data[hash];
+                        }))
+                        .then(function() {
+                            message = $('#comment').val();
+                            data[hash].comments.push({
+                                person: person,
+                                comment: message,
+                                date: new Date()
+                            });
+                            return Promise.resolve(messages.update(data));
+
+
+                        })
+                        .then(function() {
+                            $('#comment').val('');
+                            messages.on('child_added', function(snapshot) {
+                                console.log('child_added: ', snapshot.val());
+                                var data = snapshot.val();
+                                // console.log('data, please get here: ', data);
+                                // if (data && data[messageID]) {
+                                    var name = sdk.User.getAccountSwitcherContactList()[0].name;
+                                    // console.log('get in here please');
+                                    var last = data.comments[data.comments.length - 1];
+                                    // var comments = data[messageID][name].comments;
+                                    // comments.forEach(function(commemt) {
+                                    var comm = document.createElement('div');
+                                    var first = name.split(' ')[0];
+                                    comm.innerHTML = first + ": " + last.comment + last.date;
+                                    var addComment = document.getElementById('addComment');
+                                    if (last.date) addComment.appendChild(comm);
+                                    // })
+                                // }
+                            });
+                        })
+                })
+            })
+        })
+
+
         threadView.addSidebarContentPanel({
             el: comments,
             title: 'Comments',
             iconUrl: 'https://cdn2.iconfinder.com/data/icons/windows-8-metro-style/512/comments.png'
         })
-        var messageID;
+    })
+    var messageID;
+    sdk.Conversations.registerThreadViewHandler(function(threadView) {
         chrome.runtime.sendMessage({
             type: 'read message',
             threadId: threadView.getThreadID()
         }, function(response) {
             messageID = response;
             messages.once('value', function(snapshot) {
+
                 var data = snapshot.val();
+                console.log('data, please get here: ', data);
+                console.log(messageID);
                 if (data && data[messageID]) {
-                    console.log('data: messageID', snapshot.val());
                     var name = sdk.User.getAccountSwitcherContactList()[0].name;
-                    var comments = data[messageID][name].comments;
+                    console.log('get in here please');
+                    // var last = data[messageID][name].comments[data[messageID][name].comments.length - 1];
+                    var comments = data[messageID].comments;
                     comments.forEach(function(comment) {
                         var comm = document.createElement('div');
                         var first = name.split(' ')[0];
-                        comm.innerHTML = first + ": " + comment.message + comment.date;
+                        comm.innerHTML = first + ": " + comment.comment + comment.date;
                         var addComment = document.getElementById('addComment');
                         if (comment.date) addComment.appendChild(comm);
                     })
@@ -36,40 +96,5 @@ InboxSDK.load('1.0', 'sdk_CapstoneIDK_aa9966850e').then(function(sdk) {
             });
 
         })
-
-        $(comments).load(chrome.extension.getURL('/templates/comment.html'), function(page) {
-            var submit = document.getElementById('submit');
-            submit.addEventListener('click', function(event) {
-                event.preventDefault();
-                var foo = {};
-                person = sdk.User.getAccountSwitcherContactList()[0].name;
-                Promise.resolve(messages.on('value', function(snapshot) {
-                        var data = snapshot.val();
-                        foo[messageID] = data[messageID];
-                    }))
-                    .then(function() {
-                        message = $('#comment').val();
-                        foo[messageID][person].comments.push({
-                            message: message,
-                            date: new Date()
-                        });
-                        return Promise.resolve(messages.update(foo));
-                    })
-                    .then(function() {
-                        $('#comment').val('');
-                    })
-            })
-        })
-        messages.on('child_changed', function(snapshot) {
-            var data = snapshot.val();
-            console.log('snapshot: ', snapshot.val());
-            var name = sdk.User.getAccountSwitcherContactList()[0].name;
-            var last = data[name].comments[data[name].comments.length - 1];
-            var comm = document.createElement('div');
-            var first = name.split(' ')[0];
-            comm.innerHTML = first + ": " + last.message + last.date;
-            var addComment = document.getElementById('addComment');
-            if (last.date) addComment.appendChild(comm);
-        });
-    })
-})
+    });
+});
