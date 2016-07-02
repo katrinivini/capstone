2affcd9cdf96de1294f29644dbe32e6f1629bc37
Kathy Chang <emailkathy@gmail.com>
2016-07-02 16:13:59 -0400

    fsreadfilesync working, angular not working

--- js/threadview/assign/assign-button.js
+++ js/threadview/assign/assign-button.js
@@ -1,14 +1,47 @@
 var $ = require('jquery');
 var members = require('../../myapp.js').members;
+// This is brfs fs, not Node's fs. Need to npm install brfs.
+var fs = require('fs');
+var path = require('path')
 
-// This is modeled after BL's dashboard.
+
+// // This is modeled after BL's dashboard. Works with data retrieval from Firebase sometimes.
+// InboxSDK.load('1.0', 'sdk_CapstoneIDK_aa9966850e').then(function(sdk) {
+
+//     var el = document.createElement("div");
+//     $(el).load(chrome.extension.getURL('/templates/assign-button.html'));
+//     angular.element(document).ready(function() {
+//         angular.bootstrap(el, ['shazzam'])
+//     });
+
+//     sdk.Toolbars.registerToolbarButtonForThreadView({
+//         title: 'Assign',
+//         iconUrl: 'https://cdn3.iconfinder.com/data/icons/box-and-shipping-supplies-icons/447/Clipboard_With_Pencil-512.png',
+//         section: 'METADATA_STATE',
+//         hasDropdown: false,
+//         onClick: function(event) {
+//             sdk.Widgets.showModalView({
+//                 title: 'Well hello there beautiful',
+//                 el: el
+//             })
+//         }
+//     });
+
+// });
+
+
+
+var html = fs.readFileSync(path.resolve(__dirname + '/../../../templates/assign-button.html'), 'utf8');
+
+$('assign').html(html);
+
+angular.element(document).ready(function() {
+    angular.bootstrap(html, ['shazzam'])
+});
+
+// console.log("html: ", html);
+
 InboxSDK.load('1.0', 'sdk_CapstoneIDK_aa9966850e').then(function(sdk) {
-    
-    var el = document.createElement("div");
-    $(el).load(chrome.extension.getURL('/templates/assign-button.html'));
-    angular.element(document).ready(function(){
-        angular.bootstrap(el, ['shazzam'])  
-    });
 
     sdk.Toolbars.registerToolbarButtonForThreadView({
         title: 'Assign',
@@ -18,40 +51,9 @@ InboxSDK.load('1.0', 'sdk_CapstoneIDK_aa9966850e').then(function(sdk) {
         onClick: function(event) {
             sdk.Widgets.showModalView({
                 title: 'Well hello there beautiful',
-                el: el
+                el: html
             })
         }
     });
 
 });
-
-
-
-// This is BL's working dashboard with Angular.
-// InboxSDK.load('1.0', 'sdk_CapstoneIDK_aa9966850e').then(function(sdk) {
-//     sdk.Toolbars.registerToolbarButtonForThreadView({
-//         title: 'Assign',
-//         section: 'INBOX_STATE',
-//         iconUrl: 'https://cdn0.iconfinder.com/data/icons/pixel-perfect-at-24px-volume-6/24/2140-512.png',
-//         hasDropdown: true,
-//         onClick: function(event) {
-//             var emails = [];
-//             var list = document.createElement('div');
-//             Promise.resolve(members.once('value', function(snapshot) {
-//                     var data = snapshot.val();
-//                     var properties = Object.getOwnPropertyNames(data); //returns array of enumerable property names
-//                     properties.forEach(function(prop) {
-//                         emails.push(data[prop]);
-//                     })
-//                 }))
-//                 .then(function() {
-//                     emails.forEach(function(email) {
-//                         var em = document.createElement('div');
-//                         em.innerHTML = email;
-//                         list.appendChild(em);
-//                     });
-//                     event.dropdown.el.appendChild(list);
-//                 });
-//         }
-//     });
-// });
--- js/threadview/taskhistory.js
+++ js/threadview/taskhistory.js
@@ -1,53 +1,74 @@
 var messages = require('../myapp.js').messages;
-var taskHistory;
+var taskHistory = document.createElement('div');
+var messageID;
+var Firebase = require('firebase');
+$ = require('jquery');
+// console.log('Firebase: ', Firebase);
 InboxSDK.load('1.0', 'sdk_CapstoneIDK_aa9966850e').then(function(sdk) {
     sdk.Conversations.registerThreadViewHandler(function(threadView) {
-        taskHistory = document.createElement('div');
         threadView.addSidebarContentPanel({
             el: taskHistory,
             title: 'Task History',
             iconUrl: 'https://cdn3.iconfinder.com/data/icons/website-panel-icons/128/test1-13-512.png'
         })
         chrome.runtime.sendMessage({
-            type: 'read message',
-            threadId: threadView.getThreadID()
-        }, function(hash) {
-            var person = sdk.User.getAccountSwitcherContactList()[0].name;
-            console.log('now trying to get metadata: ', hash);
-            Promise.resolve(messages.once('value', function(snapshot) {
-                    var data = snapshot.val();
-                    if (data && data[hash]) { //we have data and the thread
-                        for (var i = 0; i < data[hash].activity.length; i++) {
-                            if (data[hash].activity[i].person === person && data[hash].activity[i].action === "read") {
-                                return;
-                            }
-                        }
-                        data[hash].activity.push(eventObj(person, "read"));
-                        messages.update(data);
-                    } else { //we either don't have the data or dont have the thread
-                        if (!data) data = {};
-                        // messages.ref('/' + hash);
-                        data[hash] = {};
-                        data[hash].activity = [eventObj(person, "read")];
-                        messages.update(data);
-                    }
-                }))
-                .then(function() {
-                    messages.once('value', function(snapshot) {
-                        var data = snapshot.val();
-                        data[hash].activity.forEach(function(act) {
-                            createActivity(act.person, act.action, act.date);
-                        });
-                    });
-                })
+                type: 'read message',
+                threadId: threadView.getThreadID()
+            }, function(hash) {
+                messageID = hash;
+                var person = sdk.User.getAccountSwitcherContactList()[0].name;
+                messages.once('value', function(snapshot) { //this is a promise
+                        console.log('this is when you read the message: ', snapshot.val());
+                        var readMessages = snapshot.val();
+                        if (readMessages && readMessages[hash]) { //we have readMessages and the thread
+                            var people = Array.prototype.slice.call(readMessages[hash].people)
+                            var index = people.indexOf(person);
+                            console.log('index: ', index); //{
+                            if (index !== -1 && people[index].status == 'read') return;
+                            // }
+                            console.log('do i get in here???');
 
-        })
-        messages.on('child_changed', function(snapshot) {
-            var data = snapshot.val();
-            var last = data.activity[data.activity.length - 1];
-            if (last.date) createActivity(last.person, last.action, last.date);
-            console.log('someone please read my email: ', snapshot.val());
-        })
+                            readMessages[hash].activity.push(eventObj(person, "read"));
+                            readMessages[hash].people.push({
+                                person: person,
+                                status: 'read'
+                            })
+                            if ($(taskHistory).children()) $(taskHistory).children().remove();
+                            readMessages[hash].activity.forEach(function(task) {
+                                    var date = new Date(task.date).getTime();
+                                    createActivity(task.person, task.action, date)
+                            })
+                        } else { //we either don't have the readMessages or dont have the thread
+                            if (!readMessages) readMessages = {};
+                            // messages.ref('/' + hash);
+                            readMessages[hash] = {
+                                activity: [eventObj(person, "read")],
+                                people: [{
+                                    person: person,
+                                    status: 'read'
+                                }]
+                            };
+                        }
+                        return messages.update(readMessages);
+                    })
+                    .then(function() {
+                        messages.child(messageID).child('activity').on('child_added', function(snapshot) {
+                            var task = snapshot.val();
+                            console.log('new task from task history: ', task);
+                            // data[hash].activity.forEach(function(act) {
+                            var date = new Date(task.date).getTime();
+                            createActivity(task.person, task.action, date);
+                            // });
+                        });
+                    })
+
+            })
+            // messages.child(messageID).on('child_added', function(snapshot) {
+            //     console.log('this is when you want to listen for an addition to the task history: ', snapshot.val());
+            //     var data = snapshot.val();
+            //     // var last = data.activity[data.activity.length - 1];
+            //     // if (last.date) createActivity(last.person, last.action, last.date);
+            // })
     });
 });
 
@@ -58,7 +79,7 @@ function eventObj(p, a) {
     return {
         person: p,
         action: a,
-        date: new Date()
+        date: Firebase.database.ServerValue.TIMESTAMP
     }
 }
 
@@ -67,3 +88,116 @@ function createActivity(person, action, date) {
     act.innerHTML = person + " " + action + " " + date;
     taskHistory.appendChild(act);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // OLD VERSION
+
+// var messages = require('../myapp.js').messages;
+// var taskHistory;
+// InboxSDK.load('1.0', 'sdk_CapstoneIDK_aa9966850e').then(function(sdk) {
+//     sdk.Conversations.registerThreadViewHandler(function(threadView) {
+//         taskHistory = document.createElement('div');
+//         threadView.addSidebarContentPanel({
+//             el: taskHistory,
+//             title: 'Task History',
+//             iconUrl: 'https://cdn3.iconfinder.com/data/icons/website-panel-icons/128/test1-13-512.png'
+//         })
+//         chrome.runtime.sendMessage({
+//             type: 'read message',
+//             threadId: threadView.getThreadID()
+//         }, function(hash) {
+//             var person = sdk.User.getAccountSwitcherContactList()[0].name;
+//             console.log('now trying to get metadata: ', hash);
+//             Promise.resolve(messages.once('value', function(snapshot) {
+//                     var data = snapshot.val();
+//                     if (data && data[hash]) { //we have data and the thread
+//                         for (var i = 0; i < data[hash].activity.length; i++) {
+//                             if (data[hash].activity[i].person === person && data[hash].activity[i].action === "read") {
+//                                 return;
+//                             }
+//                         }
+//                         data[hash].activity.push(eventObj(person, "read"));
+//                         messages.update(data);
+//                     } else { //we either don't have the data or dont have the thread
+//                         if (!data) data = {};
+//                         // messages.ref('/' + hash);
+//                         data[hash] = {};
+//                         data[hash].activity = [eventObj(person, "read")];
+//                         messages.update(data);
+//                     }
+//                 }))
+//                 .then(function() {
+//                     messages.once('value', function(snapshot) {
+//                         var data = snapshot.val();
+//                         data[hash].activity.forEach(function(act) {
+//                             createActivity(act.person, act.action, act.date);
+//                         });
+//                     });
+//                 })
+
+//         })
+//         messages.on('child_changed', function(snapshot) {
+//             var data = snapshot.val();
+//             var last = data.activity[data.activity.length - 1];
+//             if (last.date) createActivity(last.person, last.action, last.date);
+//             console.log('someone please read my email: ', snapshot.val());
+//         })
+//     });
+// });
+
+// // thread > activity and comments
+// // activity > [{person: person, action: action, date: date}]
+// // comment > [{person:person , comment:comment , date:date}]
+// function eventObj(p, a) {
+//     return {
+//         person: p,
+//         action: a,
+//         date: new Date()
+//     }
+// }
+
+// function createActivity(person, action, date) {
+//     var act = document.createElement('div');
+//     act.innerHTML = person + " " + action + " " + date;
+//     taskHistory.appendChild(act);
+// }
