16fbc5ea8a81c70eb6747cbbf422e72fdf3e4d7e
Kathy Chang <emailkathy@gmail.com>
2016-07-03 05:13:12 -0400

    Updates db if msgID exists. Not getting input value or task history template.

--- angular/app.js
+++ angular/app.js
@@ -1,4 +1,7 @@
-// var $ = require('jquery');
+var $ = require('jquery');
+var Firebase = require('firebase');
+
+
 var app = angular.module('thing', ['firebase', 'ui.router']);
 
 app.controller('DashboardCtrl', function($scope) {
@@ -14,19 +17,99 @@ var assignapp = angular.module('shazzam', ['firebase']);
 assignapp.controller('AssignCtrl', function($scope, $firebaseArray) {
 	console.log("inside assignapp.js AssignCtrl");
 
+	var member;
+	var messageID;
+	var threadID;
+	var readMessages;
+	var taskHistory = document.getElementsByClassName('taskHistory')[0];
+	var messages = firebase.database().ref('/messages');
+	var ref = firebase.database().ref('/members');
+
+	function eventObj(p, a) {
+		return {
+			person: p,
+            action: a,
+            date: Firebase.database.ServerValue.TIMESTAMP
+        }
+    }
+
+    function createActivity(person, action, date) {
+        var act = document.createElement('div');
+        act.innerHTML = person + " " + action + " " + date;
+        taskHistory.appendChild(act);
+    }
+
+    console.log("taskHistory: ", taskHistory);
+
+
+	InboxSDK.load('1.0', 'sdk_CapstoneIDK_aa9966850e').then(function(sdk) {
+		member = sdk.User.getAccountSwitcherContactList()[0].name;
+
+		sdk.Conversations.registerThreadViewHandler(function(threadView) {
+			threadID = threadView.getThreadID();
+
+			chrome.runtime.sendMessage({
+				type: 'read message',
+				threadId: threadID
+	        }, function(hash) { 
+	        	messageID = hash;
+	        	console.log("AssignCtrl member name: ", member);
+				console.log("AssignCtrl threadID: ", threadID);
+				console.log("AssignCtrl messageID: ", messageID);
+
+				messages.once('value', function(snapshot) {
+					readMessages = snapshot.val();
+		        })
+				.then(function() {
+			console.log("messageID: ", messageID);
+			messages.child(messageID).child('activity').on('child_added', function(snapshot) {
+				var task = snapshot.val();
+				var date = new Date(task.date);
+				createActivity(task.person, task.action, date);
+		    });
+		})
+	        
+
+
+	        })
+	        })
+
+	})
+
+
+
+
+
+
+	
 	$scope.members = [];
-	var ref = firebase.database().ref('/members'); 
 	var arr = $firebaseArray(ref);
 	arr.$loaded().then(function(data) {
 		angular.forEach(arr, function(item) {
 			$scope.members.push(item.$value);
 		})
 	});
-	
+
+
+
+	$scope.member = 'Elmo';
+
 	$scope.assignsubmit = function() {
-		console.log("you assignsubmitted something but not really");
+
+		console.log("assignee: ", $scope.member);
+
+
+		readMessages[messageID].activity.push(eventObj(member, "assigned to " + $scope.member));
+		readMessages[messageID].people.push({person: member, status: "assigned"});
+
+		messages.update(readMessages);
+
+		console.log("you maybe assignsubmitted something");
 	}
-});
+});    // end of controller
+
+
+
 
 // var insert = '';
 // // var el = document.createElement("div");
