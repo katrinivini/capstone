9a44fb63c87b215685fe4d7ec9f5ad3afbdfc6e0
Kathy Chang <emailkathy@gmail.com>
2016-07-06 14:00:14 -0400

    Updates Firebase messages with assignment. Adds own Gmail label. Adds own Gmail threadID to messages. Lists messages matching query. Label IDs still hard-coded.

--- angular/app.js
+++ angular/app.js
@@ -5,8 +5,8 @@ var Firebase = require('firebase');
 var app = angular.module('thing', ['firebase', 'ui.router']);
 
 app.controller('DashboardCtrl', function($scope) {
-	$scope.kathy = "kathy yay";
-	console.log("hello from inside app.js DashboardCtrl");
+    $scope.kathy = "kathy yay";
+    console.log("hello from inside app.js DashboardCtrl");
 });
 
 
@@ -16,83 +16,139 @@ app.controller('DashboardCtrl', function($scope) {
 var assignapp = angular.module('shazzam', ['firebase']);
 
 assignapp.controller('AssignCtrl', function($scope, $firebaseArray) {
-	console.log("inside assignapp.js AssignCtrl");
+    console.log("inside assignapp.js AssignCtrl");
 
-	var member;
-	var messageID;
-	var threadID;
-	var readMessages;
-	var messages = firebase.database().ref('/messages');
-	var members = firebase.database().ref('/members');
+    var member;
+    var messageID;
+    var threadID;
+    var readMessages;
+    var assignedThreads;
+    var assignments = firebase.database().ref('/assignments');
+    var messages = firebase.database().ref('/messages');
+    var members = firebase.database().ref('/members');
 
 
-	// This came from taskhistory.js.
-	function eventObj(p, a) {
-		return {
-			person: p,
-            action: a,
+    // This came from taskhistory.js.
+    function assignment(assigner, assignee) {
+        return {
+            person: assigner,
+            action: "assigned to " + assignee,
+            assignee: assignee,
             date: Firebase.database.ServerValue.TIMESTAMP
         }
     }
 
     // Load InboxSDK.
-	InboxSDK.load('1.0', 'sdk_CapstoneIDK_aa9966850e').then(function(sdk) {
+    InboxSDK.load('1.0', 'sdk_CapstoneIDK_aa9966850e').then(function(sdk) {
 
-		// Get name of user who's viewing thread.
-		member = sdk.User.getAccountSwitcherContactList()[0].name;
+            // Get name of user who's viewing thread.
+            member = sdk.User.getAccountSwitcherContactList()[0].name;
 
-		// Register ThreadViewHandler to get threadID.
-		sdk.Conversations.registerThreadViewHandler(function(threadView) {
+            // Register ThreadViewHandler to get threadID.
+            sdk.Conversations.registerThreadViewHandler(function(threadView) {
 
-			threadID = threadView.getThreadID();
+                    threadID = threadView.getThreadID();
 
-			// Use threadID to call gapi in background script to get messageID.
-			// Because messageID can only come from gapi.
-			chrome.runtime.sendMessage({
-				type: 'read message',
-				threadId: threadID    // Watch out. Case-sensitive.
-	        }, function(hash) { 
-	        	// Hash function in background gets rid of characters Firebase doesn't like.
-	        	messageID = hash;
+                    // Use threadID to call gapi in background script to get messageID.
+                    // Because messageID can only come from gapi.
+                    chrome.runtime.sendMessage({
+                            type: 'read message',
+                            threadId: threadID // Watch out. Case-sensitive.
+                        }, function(hash) {
+                            // Hash function in background gets rid of characters Firebase doesn't like.
+                            messageID = hash;
 
-	        	// Get all messageIDs (messages get added to Firebase when they're read).
-				messages.once('value', function(snapshot) {
-					readMessages = snapshot.val();
-		        });
-
-	        })    // closes sendMessage
-	    })    // closes registerThreadViewHandler
-	})    // closes InboxSDK then
+                            // Get all messageIDs (messages get added to Firebase when they're read).
+                            messages.once('value', function(snapshot) {
+                                readMessages = snapshot.val();
+                            });
 
 
 
-	// Add members from Firebase to Angular scope.
-	// $loaded is a Firebase thing.
-	$scope.members = [];
-	var membersArray = $firebaseArray(members);
-	membersArray.$loaded().then(function(data) {
-		angular.forEach(membersArray, function(item) {
-			$scope.members.push(item.$value);
-		})
-	});
+                        }) // closes sendMessage
+                }) // closes registerThreadViewHandler
+        }) // closes InboxSDK then
 
-	// Adds assignment activity to Firebase.
-	// DOM updates via whatever's in taskhistory.js.
-	$scope.submitAssignment = function() {
 
-		// Gets radio input value. Used jquery because I gave up on Angular.
-		// Although you need ng-value (not just value) in the template for this to work. Life's a mystery.
-		var assignee = $('input[name=radioMember]:checked', '#assignForm').val();
+	
 
-		// Adds assignment to Firebase.
-		readMessages[messageID].activity.push(eventObj(member, "assigned to " + assignee));
-		readMessages[messageID].people.push({person: member, status: "assigned"});
 
-		// Saves updates.
-		messages.update(readMessages);
+    // Add members from Firebase to Angular scope.
+    // $loaded is a Firebase thing.
+    $scope.members = [];
+    var membersArray = $firebaseArray(members);
+    membersArray.$loaded().then(function(data) {
+        angular.forEach(membersArray, function(item) {
+            $scope.members.push(item.$value);
+        })
+    });
 
-	}
-});    // end of controller
+    // Adds assignment activity to Firebase.
+    // DOM updates via whatever's in taskhistory.js.
+    $scope.submitAssignment = function() {
+
+        var labelID;
+
+        // Gets radio input value. Used jquery because I gave up on Angular.
+        // Although you need ng-value (not just value) in the template for this to work. Life's a mystery.
+        var assignee = $('input[name=radioMember]:checked', '#assignForm').val();
+
+        switch (assignee) {
+            case "b.emma.lai@gmail.com":
+                labelID = "Label_16";
+                break;
+            case "emailkathy@gmail.com":
+                labelID = "Label_17";
+                break;
+            case "rina.krevat@gmail.com":
+                labelID = "Label_18";
+                break;
+            case "katrinamvelez@gmail.com":
+                labelID = "Label_19";
+                break;
+        }
+
+        // Adds assignment to Firebase.
+        readMessages[messageID].activity.push(assignment(member, assignee));
+        readMessages[messageID].people.push({ person: member, status: "assigned" });
+
+
+        // Make gapi call to add label.
+        chrome.runtime.sendMessage({
+            type: 'add label',
+            threadId: threadID,
+            labelsToAdd: [labelID],
+            labelsToRemove: []
+        }, function(gapiResponse) {
+
+        	
+            
+            console.log("background response to add label: ", gapiResponse);
+        });
+
+        // Make gapi call to get list of user's labels.
+        chrome.runtime.sendMessage({
+            type: 'list labels'
+        }, function(gapiResponse) {
+            console.log("background response to list labels (labelDictionary): ", gapiResponse);
+        });
+
+
+        if (!readMessages[messageID].gmailThreadIDs) readMessages[messageID].gmailThreadIDs = {};
+        readMessages[messageID]["gmailThreadIDs"][member] = threadID;
+
+
+
+        // Saves updates.
+        messages.update(readMessages);
+
+
+
+
+
+
+    }
+}); // end of controller
 
 
 
@@ -111,71 +167,71 @@ assignapp.controller('AssignCtrl', function($scope, $firebaseArray) {
 
 // })
 
-app.config(function($urlRouterProvider, $locationProvider, $stateProvider){
-// 	// This turns off hashbang urls (/#about) and changes it to something normal (/about)
-	// $locationProvider.html5Mode(true);
-// 	// If we go to a URL that ui-router doesn't have registered, go to the "/" url.
-	// $urlRouterProvider.otherwise('/userpanel');
+app.config(function($urlRouterProvider, $locationProvider, $stateProvider) {
+    // 	// This turns off hashbang urls (/#about) and changes it to something normal (/about)
+    // $locationProvider.html5Mode(true);
+    // 	// If we go to a URL that ui-router doesn't have registered, go to the "/" url.
+    // $urlRouterProvider.otherwise('/userpanel');
 
-	// $urlRouterProvider.when('/thing', '/userpanel');
+    // $urlRouterProvider.when('/thing', '/userpanel');
 
-	$stateProvider.state('userpanel', {
-		url: '/userpanel', 
-		controller: 'UserPanelCtrl'
-	})
+    $stateProvider.state('userpanel', {
+        url: '/userpanel',
+        controller: 'UserPanelCtrl'
+    })
 
-	$stateProvider.state('dashboard', {
-		url: '/dashboard',
-		controller: 'DashboardCtrl'
-	})
+    $stateProvider.state('dashboard', {
+        url: '/dashboard',
+        controller: 'DashboardCtrl'
+    })
 
-	$stateProvider.state('shared-labels', {
-		url: '/shared-labels',
-		controller: 'LabelsCtrl'
-	})
+    $stateProvider.state('shared-labels', {
+        url: '/shared-labels',
+        controller: 'LabelsCtrl'
+    })
 
-	$stateProvider.state('shared-contacts', {
-		url: '/shared-contacts',
-		controller: 'ContactsCtrl'
-	})
+    $stateProvider.state('shared-contacts', {
+        url: '/shared-contacts',
+        controller: 'ContactsCtrl'
+    })
 
-	$stateProvider.state('email-templates', {
-		url: '/email-templates',
-		controller: 'TemplatesCtrl'
-	})
+    $stateProvider.state('email-templates', {
+        url: '/email-templates',
+        controller: 'TemplatesCtrl'
+    })
 
-	$stateProvider.state('email-templates.create', {
-		url: '/create',
-		controller: 'TemplatesCtrl'
-	})
+    $stateProvider.state('email-templates.create', {
+        url: '/create',
+        controller: 'TemplatesCtrl'
+    })
 
-	$stateProvider.state('settings', {
-		url: '/settings',
-		controller: 'SettingsCtrl'
-	})
+    $stateProvider.state('settings', {
+        url: '/settings',
+        controller: 'SettingsCtrl'
+    })
 
 })
 
 app.controller('UserPanelCtrl', function($scope, $firebaseArray, $state) {
-	
+
 });
 
 app.controller('DashboardCtrl', function($scope, $firebaseArray, $state) {
-	
+
 });
 
 app.controller('LabelsCtrl', function($scope, $firebaseArray, $state) {
-	
+
 });
 
 app.controller('ContactsCtrl', function($scope, $firebaseArray, $state) {
-	
+
 });
 
 app.controller('TemplatesCtrl', function($scope, $firebaseArray, $state) {
-	
+
 });
 
 app.controller('SettingsCtrl', function($scope, $firebaseArray, $state) {
-	
+
 });
