93cbdcf8ad8cae5a5b427224c1e4c9d5887810f6
Katrina Velez <katrinamvelez@gmail.com>
2016-07-08 17:29:25 -0400

    getting snooozed items to appear on dashboard again

--- angular/snoozed/snoozed.controller.js
+++ angular/snoozed/snoozed.controller.js
@@ -31,23 +31,26 @@ chrome.runtime.sendMessage({
 
 app.controller('SnoozeCtrl', function($scope, $firebase, $firebaseArray, $state) {
     var snoozedemails = [];
-    // $scope.snoozedEmails = [];
+    $scope.snoozedemails = [];
     // $scope.hello = 'kathy';
     console.log('index: ', index);
     members.child(index).child('activity').once('value', function(snapshot) {
         var userHistory = snapshot.val();
+        console.log('members once for activity history', userHistory);
         for (var id in userHistory) {
-            if (userHistory[id].action === 'snoozed') {
-                snoozedemails.push(userHistory[id]);
+            if (userHistory.hasOwnProperty(id) && userHistory[id].action === 'snoozed') {
+            	console.log('get into this if statement for user history');
+                snoozedemails.push({databaseId: id , userHistory: userHistory[id]});
             }
         }
-        $scope.snoozedemails = [];
+        console.log('snoozedemails: ', snoozedemails);
         snoozedemails.forEach(function(snoozedemail) {
             chrome.runtime.sendMessage({
                 type: 'fetch email',
-                threadId: snoozedemail.threadId
+                threadId: snoozedemail.userHistory.threadId
             }, function(emailBody) {
-                $scope.snoozedemails.push({ threadId: snoozedEmail.threadId, body: emailBody.snippet, date: new Date(snoozedemail.date) });
+            	console.log('hmmmm, does the email body get here', emailBody);
+                $scope.snoozedemails.push({ databaseId: snoozedemail.databaseId, threadId: snoozedemail.threadId, body: emailBody.snippet, date: new Date(snoozedemail.userHistory.date) });
                 console.log('this is a snoozed email: ', emailBody);
                 console.log('$scope.snoozedEmails: ', $scope.snoozedemails);
                 $scope.$digest();
@@ -56,8 +59,19 @@ app.controller('SnoozeCtrl', function($scope, $firebase, $firebaseArray, $state)
 
     })
 
-    $scope.removeSnooze = function(threadId){
+    $scope.removeSnooze = function(databaseId){
     	//have to delete thing from database, then call scope.digest
+    	console.log('removing snooze');
+    	members.child(index).child('activity').child(databaseId).remove();
+    	console.log(members.child(index).child('activity').child(databaseId).remove())
+    	.then(function(){
+    		$scope.digest();
+    	})
+    	// $scope.$digest();
+    	// members.child(index).child('activity').once('child_removed', function(){
+    	// 	console.log('members child activity once child removed');
+    	// 	$scope.$digest();
+    	// })
     }
 
 });
