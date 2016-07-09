009a683b00f204f3e91403b39409cafc93f20043
Kathy Chang <emailkathy@gmail.com>
2016-07-09 17:14:55 -0400

    Cleaned up app.js

--- angular/app.js
+++ angular/app.js
@@ -41,8 +41,6 @@ var $ = require('jquery');
 
 assignapp.controller('AssignCtrl', function($scope, $firebaseArray) {
 
-    var assignedThreads;
-
     // This came from taskhistory.js.
     function assignment(assigner, assignee) {
         return {
@@ -74,61 +72,59 @@ assignapp.controller('AssignCtrl', function($scope, $firebaseArray) {
     // Load InboxSDK.
     InboxSDK.load('1.0', 'sdk_CapstoneIDK_aa9966850e').then(function(sdk) {
 
-            // Get name of user who's viewing thread.
-            member = sdk.User.getAccountSwitcherContactList()[0].name;
+        // Get name of user who's viewing thread.
+        member = sdk.User.getAccountSwitcherContactList()[0].name;
 
-            // Refreshes Inbox whenever navigating to Inbox view.
-            // So that assignment labels show up right after returning to Inbox.
-            // Otherwise you have to wait a while or press the refresh button.
-            var route = sdk.Router.NativeListRouteIDs.INBOX;
-            sdk.Router.handleListRoute(route, function(inboxView) {
-                inboxView.refresh();
-                console.log("Inbox refreshed!");
-            });
+        // Refreshes Inbox whenever navigating to Inbox view.
+        // So that assignment labels show up right after returning to Inbox.
+        // Otherwise you have to wait a while or press the refresh button.
+        var route = sdk.Router.NativeListRouteIDs.INBOX;
+        sdk.Router.handleListRoute(route, function(inboxView) {
+            inboxView.refresh();
+            console.log("Inbox refreshed!");
+        });
 
-            // Register ThreadViewHandler to get threadID.
-            sdk.Conversations.registerThreadViewHandler(function(threadView) {
+        // Register ThreadViewHandler to get threadID.
+        sdk.Conversations.registerThreadViewHandler(function(threadView) {
 
-                    threadID = threadView.getThreadID();
+                threadID = threadView.getThreadID();
 
-                    // Use threadID to call gapi in background script to get messageID.
-                    // Because messageID can only come from gapi.
-                    chrome.runtime.sendMessage({
-                            type: 'read message',
-                            threadId: threadID // Watch out. Case-sensitive.
-                        }, function(hash) {
-                            // Hash function in background gets rid of characters Firebase doesn't like.
-                            messageID = hash;
-                            // Get all messageIDs (messages get added to Firebase when they're read).
-                            messages.once('value', function(snapshot) {
-                                readMessages = snapshot.val();
-                            });
+                // Use threadID to call gapi in background script to get messageID.
+                // Because messageID can only come from gapi.
+                chrome.runtime.sendMessage({
+                        type: 'read message',
+                        threadId: threadID // Watch out. Case-sensitive.
+                    }, function(hash) {
+                        // Hash function in background gets rid of characters Firebase doesn't like.
+                        messageID = hash;
+                        // Get all messageIDs (messages get added to Firebase when they're read).
+                        messages.once('value', function(snapshot) {
+                            readMessages = snapshot.val();
+                        });
 
-                        }); // closes sendMessage
-                }); // closes registerThreadViewHandler
+                    }); // closes sendMessage
+            }); // closes registerThreadViewHandler
 
-            sdk.Toolbars.registerToolbarButtonForThreadView({
-                title: 'Assign',
-                iconUrl: 'https://cdn3.iconfinder.com/data/icons/box-and-shipping-supplies-icons/447/Clipboard_With_Pencil-512.png',
-                section: 'METADATA_STATE',
-                hasDropdown: false,
-                onClick: function(event) {
-                    modalAssignView = sdk.Widgets.showModalView({
-                        title: '',
-                        el: el
-                    });
-                }
-            });  // closes registerToolbarButtonForThreadView
-        }) // closes InboxSDK then
+        sdk.Toolbars.registerToolbarButtonForThreadView({
+            title: 'Assign',
+            iconUrl: 'https://cdn3.iconfinder.com/data/icons/box-and-shipping-supplies-icons/447/Clipboard_With_Pencil-512.png',
+            section: 'METADATA_STATE',
+            hasDropdown: false,
+            onClick: function(event) {
+                modalAssignView = sdk.Widgets.showModalView({
+                    title: '',
+                    el: el
+                });
+            }
+        });  // closes registerToolbarButtonForThreadView
+    }) // closes InboxSDK then
 
-        // Add members from Firebase to Angular scope.
-        // $loaded is a Firebase thing.
-
+    // Add members from Firebase to Angular scope.
+    // $loaded is a Firebase thing.
     $scope.members = [];
     var membersArray = $firebaseArray(members);
     membersArray.$loaded().then(function(data) {
         angular.forEach(membersArray, function(item) {
-
             $scope.members.push(item.name);
         });
     });
@@ -147,42 +143,21 @@ assignapp.controller('AssignCtrl', function($scope, $firebaseArray) {
         console.log('assignee: ', assignee);
         console.log('member: ', member);
         messages.child(messageID).child('activity').push(assignment(member, assignee));
-        // members.child(assignee).push({ action: 'was assigned by' + member, threadId: threadID, date: firebase.database.ServerValue.TIMESTAMP });
+
+        // Closes modal. Unclear where modalAssignView comes from.
         modalAssignView.close();
 
-        // Makes gapi call to add label.
+        // Makes gapi call to add label upon assignment.
         chrome.runtime.sendMessage({
             type: 'add assign label',
             threadId: threadID,
             assignee: assignee,
             labelsToRemove: []
         }, function(gapiResponse) {
-
+            // Currently background doesn't send a response to this.
             console.log("background response to add assign label: ", gapiResponse);
 
         });
 
-        // // Makes gapi call to get list of user's labels.
-        // chrome.runtime.sendMessage({
-        //     type: 'list user labels'
-        // }, function(gapiResponse) {
-
-        //     console.log("background response to list user labels: ", gapiResponse);
-
-        // });
-
-        // if (!readMessages[messageID].gmailThreadIDs) readMessages[messageID].gmailThreadIDs = {};
-        // readMessages[messageID]["gmailThreadIDs"][member] = threadID;
-
-        // // Saves updates.
-        // messages.update(readMessages);
-
-        // replaces above
-
-        // messages.child(messageID).child('gmailThreadIDs').push({ member: member, threadId: threadID });
-
-
-
     }  // closes $scope.submitAssignment
-
 }); // closes assignapp.controller
